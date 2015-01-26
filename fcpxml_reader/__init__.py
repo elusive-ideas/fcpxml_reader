91a21ed830bc1ba33f7f91c5ae2f981235915da8
David Martinez <david.martinez.anim@gmail.com>
2015-01-26 16:16:57 +0000

    Added "get_frames_from_time" function

--- fcpxml_reader/__init__.py
+++ fcpxml_reader/__init__.py
@@ -1,1 +1,6 @@
-pass
+def get_frames_from_time(seconds, fps):
+    ''' Returns the numer of frames in the amount of
+    seconds specified using the framerate specified.
+
+    '''
+    return int(seconds * fps)
