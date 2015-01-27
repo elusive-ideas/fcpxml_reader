b09b933a5957eb09b9eadb85344761c5d6eacf04
David Martinez <david.martinez.anim@gmail.com>
2015-01-27 16:25:54 +0000

    Added functionality to get framerate and and its tests

--- fcpxml_reader/__init__.py
+++ fcpxml_reader/__init__.py
@@ -1,6 +1,54 @@
+from xml.etree.ElementTree import parse
+
+
 def get_frames_from_time(seconds, fps):
     ''' Returns the numer of frames in the amount of
     seconds specified using the framerate specified.
 
     '''
     return int(seconds * fps)
+
+
+def timevalue_to_seconds(duration_str):
+    ''' Providing a time division ex: "14260543/90000s" or a time expression
+    ex: "5s", returns the amount of seconds as an integer.
+
+    '''
+
+    if duration_str.endswith('s'):
+        duration_str = duration_str[:-1]
+
+    values = duration_str.split('/')
+    if len(values) > 1:
+        val01, val02 = values
+        val01 = int(val01)
+        val02 = int(val02)
+        val = None
+
+        if val01 > val02:
+            val = val01/val02
+        else:
+            val = val02/val01
+        return val
+
+    return float(values[0])
+
+
+class fcpxml_wrapper(object):
+    def __init__(self, filename=None):
+        self.framerate = 0
+        self.clip_name = None
+        self.cuts = []
+        self.speeds = []
+
+        self.read_file(filename)
+
+    def read_file(self, filename):
+        tree = parse(filename)
+        root = tree.getroot()
+        project = root.find('project')
+        resources = project.find('resources')
+        mformat = resources.find('format')
+
+        # Get the framerate
+        self.framerate = timevalue_to_seconds(mformat.get('frameDuration'))
--- run_tests.py
+++ run_tests.py
@@ -1,19 +1,91 @@
 import unittest
 import fcpxml_reader
+import os
+
+print __file__
 
 
-class test_get_frames_from_time(unittest.TestCase):
-    def test_30fps(self):
-        ''' Checks 4 seconds at 30 fps - 120 frames
-        '''
+class test_get_frames_from_seconds(unittest.TestCase):
+    def test_4sec_to_frames_30fps(self):
         frames = fcpxml_reader.get_frames_from_time(4, 30)
         self.assertEqual(frames, 120)
 
-    def test_25fps(self):
-        ''' Checks 4 seconds at 25 fps - 100 frames
-        '''
+    def test_4sec_to_frames_25fps(self):
         frames = fcpxml_reader.get_frames_from_time(4, 25)
         self.assertEqual(frames, 100)
+
+
+class test_framerates(unittest.TestCase):
+    def setUp(self):
+        self.data_folder = os.getcwd() + r'\data'
+
+    # Testing the files that are supposed to be 25 fps
+    def test_25_cut_3_clip_100_50_50(self):
+        fname = self.data_folder + r'\25fps\cut\3_clip_100_50_50.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 25)
+
+    def test_25_nocut_1_clip_25(self):
+        fname = self.data_folder + r'\25fps\no cut\1_clip_25.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 25)
+
+    def test_25_nocut_1_clip_50(self):
+        fname = self.data_folder + r'\25fps\no cut\1_clip_50.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 25)
+
+    def test_25_nocut_1_clip_100(self):
+        fname = self.data_folder + r'\25fps\no cut\1_clip_100.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 25)
+
+    def test_25_nocut_1_clip_150(self):
+        fname = self.data_folder + r'\25fps\no cut\1_clip_150.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 25)
+
+    def test_25_nocut_1_clip_200(self):
+        fname = self.data_folder + r'\25fps\no cut\1_clip_200.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 25)
+
+    # Testing the files that are supposed to be 25 fps
+    def test_30_cut_3_clip_100_50_50(self):
+        fname = self.data_folder + r'\30fps\cut\3_clip_100_50_50.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
+    def test_30_cut_3_clip_100_200_200(self):
+        fname = self.data_folder + r'\30fps\cut\3_clip_100_200_200.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
+    def test_30_nocut_1_clip_25(self):
+        fname = self.data_folder + r'\30fps\no cut\1_clip_25.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
+    def test_30_nocut_1_clip_50(self):
+        fname = self.data_folder + r'\30fps\no cut\1_clip_50.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
+    def test_30_nocut_1_clip_100(self):
+        fname = self.data_folder + r'\30fps\no cut\1_clip_100.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
+    def test_30_nocut_1_clip_150(self):
+        fname = self.data_folder + r'\30fps\no cut\1_clip_150.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
+    def test_30_nocut_1_clip_200(self):
+        fname = self.data_folder + r'\30fps\no cut\1_clip_200.fcpxml'
+        wrapper = fcpxml_reader.fcpxml_wrapper(fname)
+        self.assertEqual(wrapper.framerate, 30)
+
 
 if __name__ == '__main__':
     unittest.main()
